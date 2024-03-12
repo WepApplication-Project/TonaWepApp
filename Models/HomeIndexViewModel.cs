919900f197af8ec193561ca836296d510334a7b9
Kantinan Boonchalee <kantinan.b@lakeikeda.com>
2024-03-12 15:53:04 +0700

    feat: homeIndexViewModel and response this model to Home/Index

---
+++ Models/HomeIndexViewModel.cs
@@ -0,0 +1,9 @@
+namespace TonaWebApp.Models
+{
+    public class HomeIndexViewModel
+    {
+        public User? User { get; set; } = null!;
+
+        public List<Board> Boards { get; set; } = [];
+    }
+}
